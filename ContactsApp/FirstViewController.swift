a0339853d237a2a601f46ccb3051527770f4075f
Rashad Abdul-Salaam <rashad@getskinclick.com>
2017-07-26 18:28:17 -0400

    Initial Commit

---
+++ ContactsApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ContactsApp
+//
+//  Created by Rashad Abdul-Salaam on 7/26/17.
+//  Copyright © 2017 Rashad, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

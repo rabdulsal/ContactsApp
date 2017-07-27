39dbcc1772d8306285eb99fb0809b83a1f8bf1f7
Rashad Abdul-Salaam <rashad@getskinclick.com>
2017-07-26 23:10:03 -0400

    Edit Contact Auto-layout

---
+++ ContactsApp/ContactTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContactTableViewCell.swift
+//  ContactsApp
+//
+//  Created by Rashad Abdul-Salaam on 7/26/17.
+//  Copyright © 2017 Rashad, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactTableViewCell : UITableViewCell {
+    
+}
